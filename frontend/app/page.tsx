4a3787bd5f516b768bd4b7e86e3fe97ae56228c8
Ippei523 <65227036+Ippei523@users.noreply.github.com>
2024-04-13 21:18:52 +0900

    ヘッダーの設定

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { AppBar } from "@mui/material";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      <AppBar position="static">
+        <h1>Home</h1>
+      </AppBar>
+    </main>
   );
 }
