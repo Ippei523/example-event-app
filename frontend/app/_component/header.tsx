caa09c99777e24eb603f51c2a7587a3befeff581
Ippei523 <65227036+Ippei523@users.noreply.github.com>
2024-04-13 21:48:48 +0900

    ヘッダーの追加

--- frontend/app/_component/header.tsx
+++ frontend/app/_component/header.tsx
@@ -1,22 +1,17 @@
-import { MenuOutlined } from "@mui/icons-material";
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import Image from "next/image";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import person from "../_images/person.svg";
 
 export const Header = () => {
   return (
     <AppBar position="static">
-      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuOutlined />
-        </IconButton>
-        <Typography variant="h6">管理アプリ</Typography>
-        <Button color="inherit">Login</Button>
+      <Toolbar className="flex justify-between items-center">
+        {/* 仮の画像が欲しい */}
+        <Image src="/logo.svg" alt="logo" width={40} height={40} />
+        <Typography variant="h6">イベント管理</Typography>
+        <Button color="inherit">
+          <Image src={person} alt="person" width={40} height={40} />
+        </Button>
       </Toolbar>
     </AppBar>
   );
