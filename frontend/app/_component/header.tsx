ea82c2423797b6468a95c92b9cc720e5c446d352
Ippei523 <65227036+Ippei523@users.noreply.github.com>
2024-04-13 21:04:35 +0900

    headerとfooter登録

---
+++ frontend/app/_component/header.tsx
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
