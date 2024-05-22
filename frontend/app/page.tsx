225bf4c07348bca7467675c00437b6e1430b73ad
Ippei523 <65227036+Ippei523@users.noreply.github.com>
2024-05-22 18:52:34 +0900

    Footerを登録

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Header } from "./_component/header";
+import { Header } from "../component/header";
+import { Footer } from "../component/footer";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative">
       <Header />
+      <Box className="min-h-[calc(100vh-140px-60px)]"></Box>
+      <Footer />
     </main>
   );
 }
